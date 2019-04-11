bc9e043dfdd8ad620d66f4989a667b8a74044cb8
Hao-Han Chang <kazettique@gmail.com>
2019-04-11 23:27:24 +0800

    W: update directory when include cred.php

--- sidebar/__nav.php
+++ sidebar/__nav.php
@@ -1,4 +1,4 @@
-<?php include __DIR__ . "/../login/cred.php"; ?>
+<?php include "../login/cred.php"; ?>
 <link rel="stylesheet" href="../resources/bootstrap_customised/css/bootstrap.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP|Noto+Sans+KR|Noto+Sans+TC" rel="stylesheet">
